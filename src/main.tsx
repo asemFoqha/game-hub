9b1560de44320963279f182266f6283e76c77368
asemFoqha <asem.sh.foqha@gmail.com>
2023-10-23 01:52:22 -0400

    show loading skeletons

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import theme from "./theme";
 import App from "./App";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-  </ChakraProvider>
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </StrictMode>
 );
